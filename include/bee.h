d4b9e97ab764879612122c9af146523b2fac8a55
DariuszMedrala <44139709+DariuszMedrala@users.noreply.github.com>
2025-01-22 22:01:14 +0100

    Initial implemantation of bee module

---
+++ include/bee.h
@@ -0,0 +1,16 @@
+#ifndef BEE_H
+#define BEE_H
+
+#include "common.h"
+
+typedef struct {
+    int id;         // Unikalne ID pszczoły
+    int visits;     // Ile razy pszczoła odwiedziła ul
+    int maxVisits;  // Po ilu wizytach pszczoła umiera
+    int T_inHive;   // Czas przebywania pszczoły w ulu (sekundy)
+    HiveData* hive;
+} BeeArgs;
+
+void* beeWorker(void* arg);
+
+#endif
